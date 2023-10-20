0a50bbc6cb706ac920d9a264c35ec7bfb523da3a
patrykkawiak <patryk.kawiak@hotmail.com>
2023-10-20 09:31:58 +0200

    added shop page visualization

--- src/app/components/shop/shop.module.ts
+++ src/app/components/shop/shop.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ShopComponent } from './shop.component';
+import { ShopItemComponent } from './shop-item/shop-item.component';
 
 @NgModule({
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, ShopItemComponent],
   imports: [SharedModule, RouterModule],
-  exports: [ShopComponent],
+  exports: [ShopComponent, ShopItemComponent],
 })
 export class ShopModule {}
