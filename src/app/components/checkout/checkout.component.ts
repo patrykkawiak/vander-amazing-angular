22e7fb4cd06963440a2d0feffdb4907ce10c34ff
patrykkawiak <patryk.kawiak@hotmail.com>
2024-01-02 08:20:50 +0100

    s

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CardService } from 'src/app/core/Services/card.service';
+import { Card } from 'src/app/core/Types/Card.model';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  cardSub!: Subscription;
+  cardItems!: Card;
 
+  constructor(private cardService: CardService) {}
+
+  ngOnInit(): void {
+    this.cardItems = this.cardService.getItems();
+    console.log(this.cardItems);
+
+    this.cardService.toggleCard();
+  }
 }
+ 
